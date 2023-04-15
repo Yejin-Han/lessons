3ef499c78aad3654e884daaa96dc4a0f09b58d7b
Yejin-Han <yain3333@gmail.com>
2023-04-15 23:59:29 +0900

    230415 update

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { signOut } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   /*
     Redirect component는 v6부터 지원하지 않음.
     useHistory -> useNavigate로 변경되었으며 push, replace, goBack 등의 메소드도 지원하지 않는다.
   */
   const navigate = useNavigate();
   const onLogOutClick = () => {
-    authService.signOut();
+    signOut(authService);
     navigate("/");
   };
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorID", "==", userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
