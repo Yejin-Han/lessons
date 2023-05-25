de866a98f1f5bed21e63b69f27ba512b54d0f3f8
Yejin-Han <yain3333@gmail.com>
2023-05-25 21:28:26 +0900

    Action Dispatch, payload

--- redux-basics/react-redux-example/src/components/Counter.js
+++ redux-basics/react-redux-example/src/components/Counter.js
@@ -1,31 +1,46 @@
-import { useSelector, useDispatch } from "react-redux"; //만약 클래스 컴포넌트를 사용한다면 connect 훅을 import 할 것
+//import { Component } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
+
+// redux hook
+/*
+  useDispatch: 액션 실행
+  useSelector: 스토어의 상태값을 반환, 스토어의 상태값이 바뀐 경우 바뀐 상태값을 다시 가져와 컴포넌트를 렌더링 시킴.
+*/
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   /* 
-  저장소가 관리하는 데이터에 액세스 (리덕스가 관리하는 상태 => 우리가 추출하려는 상태 부분(복잡한 상태 구조 중 일부))
-  자동으로 subscription을 설정한다.
+    저장소가 관리하는 데이터에 액세스 (리덕스가 관리하는 상태 => 우리가 추출하려는 상태 부분(복잡한 상태 구조 중 일부))
+    자동으로 subscription을 설정한다.
   */
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button>Increment</button>
-        <button>Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
@@ -33,3 +48,45 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// class component를 사용한다면
+/* class Counter extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); */
--- redux-basics/react-redux-example/src/store/index.js
+++ redux-basics/react-redux-example/src/store/index.js
@@ -1,14 +1,32 @@
-import { legacy_createStore as createStore } from "redux"; //import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux"; //import { createStore } from "redux" -> redux toolkit을 쓰도록 권유;
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  } else if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
+const initState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initState, action) => {
+  // reducer에서 반환하는 객체는 기존 state와 병합되지 않고 기존 state를 덮어쓴다.
+  // 기존 state는 절대 변경해서는 안 되며 새로운 state 객체를 반환하여 재정의해야 함.
+
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
+    case "increase":
+      return {
+        counter: state.counter + action.amount, //extra payload 'amount'
+        showCounter: state.showCounter,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+    case "toggle":
+      return {
+        counter: state.counter,
+        showCounter: !state.showCounter,
+      };
   }
 
   return state;
